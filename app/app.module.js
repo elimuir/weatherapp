a1fe377fef21ba16cc9c4cd662224a7dfb3f7b2a
Eli Muir <eli.muir@gmail.com>
2016-07-01 16:56:07 -0700

    add weather forecast service

--- app/app.module.js
+++ app/app.module.js
@@ -1,2 +1,6 @@
 
-var weatherApp = angular.module('weatherApp', ['ui.router']);
+var config = {
+  "API_KEY": "3b8845b73507cc4d320b06dd37500a18"
+}
+
+var weatherApp = angular.module('weatherApp', ['serviceWeatherForecast', 'ui.router']);
